f0bed6b1fba7999ff3c956703e2dc4363089146a
Aridaniel <aridagnars@gmail.com>
2021-03-08 10:22:24 +0000

    seting up the site

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
+import webhook from './images.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h2>
+          Webhoook testing 
+        </h2>
+        <img src={webhook}  />
+        <img src={webhook}  />
+      <p>push to github and it will tell you</p>
+   
       </header>
     </div>
   );
